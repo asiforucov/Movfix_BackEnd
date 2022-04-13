d6d87225f5ae8d7e90b808def203b2455a3fd6fa
Asif <92951624+asiforucov@users.noreply.github.com>
2022-04-13 20:58:56 +0400

    add view

---
+++ Netflix/Controllers/Movie_DetailsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Netflix.Controllers
+{
+    public class Movie_DetailsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
